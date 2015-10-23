31fae13bee31c9212a5de55ee7cab10aea484840
강경완 <lk@lemonlab.co.kr>
2015-10-23 20:33:46 +0900

    Add : ChatHead LongTouchEvent and ShortTouch Event

--- app/src/main/java/com/lemonlab/sockjschatservice/ChattingService.java
+++ app/src/main/java/com/lemonlab/sockjschatservice/ChattingService.java
@@ -5,12 +5,15 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -24,12 +27,21 @@ public class ChattingService extends Service {
     private WindowManager mWindowManager;
     private SeekBar mSeekBar;
 
+    /*
+     * For Floating Button
+     */
     private float START_X, START_Y;
     private int PREV_X, PREV_Y;
     private int MAX_X = -1, MAX_Y = -1;
+    private boolean hasLongPress;
+    private LongPressClass LongPressFunction;
+    private Handler mHandler = null;
+
 
     private View.OnTouchListener mViewTouchListener = new View.OnTouchListener() {
         @Override public boolean onTouch(View v, MotionEvent event) {
+
+            Log.i("touch Event", event.getAction()+"");
             switch(event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     if(MAX_X == -1)
@@ -38,22 +50,52 @@ public class ChattingService extends Service {
                     START_Y = event.getRawY();
                     PREV_X = mParams.x;
                     PREV_Y = mParams.y;
+
+                    hasLongPress = false;
+                    CheckLongClick(0);
+
                     break;
                 case MotionEvent.ACTION_MOVE:
-                    int x = (int)(event.getRawX() - START_X);
-                    int y = (int)(event.getRawY() - START_Y);
 
-                    mParams.x = PREV_X + x;
-                    mParams.y = PREV_Y + y;
+                    if(hasLongPress) {
+                        int x = (int) (event.getRawX() - START_X);
+                        int y = (int) (event.getRawY() - START_Y);
 
-                    optimizePosition();
-                    mWindowManager.updateViewLayout(mPopupView, mParams);
+                        mParams.x = PREV_X + x;
+                        mParams.y = PREV_Y + y;
+
+                        optimizePosition();
+                        mWindowManager.updateViewLayout(mPopupView, mParams);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if(!hasLongPress){
+                        removeLongClickCallback();
+                        shortClickEvent();
+                    }
                     break;
             }
 
             return true;
         }
     };
+
+    private void shortClickEvent() {
+        mPopupView.setText("dsfsadfdsafs");
+    }
+
+    private void removeLongClickCallback() {
+        if(LongPressFunction != null)
+            mHandler.removeCallbacks(LongPressFunction);
+    }
+
+    private void CheckLongClick(int i) {
+        hasLongPress = false;
+        if(LongPressFunction == null)
+            LongPressFunction = new LongPressClass();
+
+        mHandler.postDelayed(LongPressFunction, ViewConfiguration.getLongPressTimeout());
+    }
 
     @Override
     public IBinder onBind(Intent arg0) { return null; }
@@ -66,9 +108,17 @@ public class ChattingService extends Service {
         mPopupView.setText("항상 떠있는 액티비티");
         mPopupView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         mPopupView.setTextColor(Color.BLUE);
+        mPopupView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.i("click", "click");
+                return false;
+            }
+        });
         mPopupView.setBackgroundColor(Color.argb(127, 0, 255, 255));
 
         mPopupView.setOnTouchListener(mViewTouchListener);
+        mHandler = new Handler();
 
         mParams = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -138,5 +188,19 @@ public class ChattingService extends Service {
             if(mSeekBar != null) mWindowManager.removeView(mSeekBar);
         }
         super.onDestroy();
+    }
+
+    private class LongPressClass implements Runnable{
+
+        @Override
+        public void run() {
+            if(LongClickEvent())
+                hasLongPress = true;
+        }
+    }
+
+    private boolean LongClickEvent(){
+        Log.i("Event", "Long");
+        return true;
     }
 }
